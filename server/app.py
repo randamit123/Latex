f7126013e5044b9dfb88602f6b37d9ba78b9cae7
Lauren Liu <laurenliu2@gmail.com>
2024-10-24 22:12:54 -0700

    firebase auth init

--- server/app.py
+++ server/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import firebase_admin
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, auth
+from routes.auth import auth_blueprint
+
+cred = credentials.Certificate("./auth-config.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
+
+app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
 @app.route('/')
 def root():
     return "root"
 
+
 if __name__  == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
