ffb0e1fccbd87dd63b6dbd80aa05af313ab41728
neham-04 <156238521+neham-04@users.noreply.github.com>
2024-11-14 12:52:17 -0800

    relations + generation and migration capabilities (updated scripts and config)

--- latexwebapp/src/app/db/index.ts
+++ latexwebapp/src/app/db/index.ts
@@ -5,7 +5,8 @@ import { config } from "dotenv"
 config ({ path: ".env.local" })
 
 const postgresql = neon(process.env.NEON_URL!)
-
 const latexdb = drizzle(postgresql, { logger: true })
+
+const result = await latexdb.execute('select 1')
 
 export { latexdb }
