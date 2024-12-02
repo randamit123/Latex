5d3d04e81608f5b24b651393f68e8846d2293905
Uma <uma.upasani@icloud.com>
2024-12-02 05:17:06 -0800

    working on document view

--- latexwebapp/src/app/dashboard/page.tsx
+++ latexwebapp/src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import { LogoutButton } from "../components/auth/LogoutButton";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
+import "/src/app/assets/homepage.css";
 
 export default async function Dashboard() {
     const session = await getServerSession();
@@ -10,9 +11,30 @@ export default async function Dashboard() {
         redirect("/");
     }
 
+    const myDocuments = () =>
+    {
+        
+    }
+
     return (
-        <>
-            Dashboard
-        </>
+        <div>
+          <div className="flex items-center justify-center h-screen">
+            <div className="homepage-box">
+                <div className="display-box">
+                    <h1 className="homepage-header">Document History</h1>
+                    <div className="document-view-box">
+                        <div className="document-titles">
+                            <b>Name</b>
+                            <b>Date Uploaded</b>
+                        </div>
+                        <div className="document-list-box"> 
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
     );
 }
